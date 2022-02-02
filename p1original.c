2424e659558bdceba255a07d850a3800c45ca15b
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-02-02 16:13:49 +0000

    main

--- p1original.c
+++ p1original.c
@@ -1,2 +1,30 @@
 #include<stdio.h>
+int main()
+{
+  int num ;
+  printf("enter the number\n");
+  scanf("%d\n",&num);
+  return num;
+}
 
+int add(int a,int b)
+{
+  int sum ;
+  sum=a+b;
+  return sum;
+}
+
+void output(int a,int b,int sum)
+{
+  printf("add %d and %d is %d\n"a,b,sum);
+}
+{
+  int main(num1,num2);
+  num1=input();
+  num2=input();
+
+  int sum;
+  sum=add(num1,num2);
+  output(num1,num2,sum);
+  return 0;
+ }
