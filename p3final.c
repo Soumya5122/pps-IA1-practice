334ed4169b387b1e9e92f44e4686408567539283
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-27 17:44:36 +0000

    assignment

--- p3final.c
+++ p3final.c
@@ -2,34 +2,33 @@
 int input_n()
 {
   int n;
-  printf("enter the number - n:\n");
+  printf("enter the number\n");
   scanf("%d",&n);
   return n;
 }
 int sum_n(int n)
 {
-int sum,i;
-sum=0;
-for(i=1;i<=n;i++)
-{
-  sum=sum+i;
-}
-return sum;
+  int sum=0;
+  for(int i=1;i<=n;i++)
+    {
+      sum=sum+i;
+    }
+  return sum;
 }
 void output(int n,int sum)
 {
-  int i;
-  for(i=1;i<n;i++)
-  {
-    printf("%d + ", i);
-  }
-  printf("%d = %d \n ",n,sum);
+  for(int i=1;i<n;i++)
+    {
+      printf("%d+",i);
+    }
+  printf("%d=%d",n,sum);
+  
 }
 int main()
 {
-  int num,result;
-  num=input_n();
-  result=sum_n(num);
-  output(num,result);
+  int n,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
   return 0;
 }
