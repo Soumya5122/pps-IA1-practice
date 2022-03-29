b3506c18fea6ccfea26c6c7527509bf06d96d7b7
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-29 10:46:43 +0000

    assignment

--- p6final.c
+++ p6final.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
-void input_two_string(char *a,char *b)
+void input_two_strings(char*a,char*b)
 {
-  printf("enter the values of two strings\n");
-  scanf("%s%s",a,b);
-}
-int cmp(int *a,int *b)
+  printf("enter the word:");
+  scanf("%s",a);
+  printf("enter the word:");
+  scanf("%s",b);
+} 
+int cmp(char*a,char*b)
 {
   int i;
-  for(i=0;(a[i]==b[i]);i++)
-    return a[i]-b[i];
+  for(i=0;(a[i]==b[i]);i++);
+  return a[i]-b[i];
 }
-void output(char a,char b,int result)
+void output(char*a,char*b,int ans)
 {
-  int ans;
   if(ans<0)
 {
 printf("%s is greater than %s\n",b,a);
 }
 else if(ans>0)
 {
-  printf("%s is greater than %s\n",a,b);
+  printf("%s is greter than %s\n",a,b);
 }
 else
 {
@@ -30,9 +31,8 @@ int main()
 {
   char a[20],b[20];
   int ans;
-  input_two_string(a,b);
+  input_two_strings(a,b);
   ans=cmp(a,b);
   output(a,b,ans);
   return 0;
-
-}
+  }
