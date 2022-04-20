b5e0a6e49fd1edb8a1191741ef5d4a1e9eeaac91
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-04-20 19:16:00 +0000

    revision

--- p6final.c
+++ p6final.c
@@ -1,36 +1,38 @@
 #include<stdio.h>
-void input_two_string(char *a,char *b)
+void input_two_strings(char*a,char*b)
 {
-  printf("enter the characters\n");
-  scanf("%s%s",a,b);
-}
-int strcmp(char *a,char *b)
+  printf("enter the word:");
+  scanf("%s",a);
+  printf("enter the word:");
+  scanf("%s",b);
+} 
+int cmp(char*a,char*b)
 {
   int i;
-  for(i=0;a[i]!="\0" && b[i]!="\0";i++)
-    
-      return a[i] - b[i];
-    
+  for(i=0;a[i]!='\0' && b[i]!='\0';i++);
+  return a[i]-b[i];
 }
-void output(char *a,char *b,int c)
+void output(char*a,char*b,int ans)
 {
-  
-  if(c>0)
-  {
-    printf("%s is greater than %s",b,a);
-  }
-  else if(c<0)
-  {
-    printf("%s is greater than %s",a,b);
-  }
-  else
-    printf("%s is greter than %s",b,a);
+  if(ans>0)
+{
+printf("%s is greater than %s\n",b,a);
+}
+else if(ans<0)
+{
+  printf("%s is greter than %s\n",a,b);
+}
+else
+{
+  printf("%s is greater than %s\n",b,a);
+}
 }
 int main()
 {
-  char a[21],b[30];
-  input_two_string(a,b);
-  int c=strcmp(a,b);
-  output(a,b,c);
+  char a[20],b[20];
+  int ans;
+  input_two_strings(a,b);
+  ans=cmp(a,b);
+  output(a,b,ans);
   return 0;
 }
